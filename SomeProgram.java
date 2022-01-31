3dc64b82bc85c189fcda90906e3b938a27966a1e
Robert Ziółko <ziolko@student.agh.edu.pl>
2022-01-31 22:27:30 +0100

    Add first implmentation of the program

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,7 @@
 public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git.");
+
+        System.out.println("We will be Git experts, soon.");
+
     }
-}
